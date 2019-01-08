41d4624d3e40f8f49021ed6161e3dfd28b9729ce
Peter <rankstar59@gmail.com>
2019-01-08 18:01:27 +1100

    v0.4.0-beta
    feature complete, waiting for conference details

--- firmware/bsides2019badge/bsides2019/key.h
+++ firmware/bsides2019badge/bsides2019/key.h
@@ -7,7 +7,7 @@
 
 const uint32_t master_key_size = 24;  // 0x00000018
 const uint8_t master_key_data[24] PROGMEM  = {
-    '\x75','\xb7','\x23','\x72','\x6c','\x1c','\x54','\x8f','\x7c','\xf7','\x56','\x3d','\x43','\x7e','\x85','\xda', // u.#rl.T.|.V=C~..
-    '\x6d','\x5c','\xb0','\x82','\xff','\xb8','\x59','\xbf',};
+    '\x97','\xa9','\x60','\x0c','\x6c','\x1c','\x54','\x8f','\x7c','\xf7','\x56','\x3d','\x43','\x7e','\x85','\xda', // ..`.l.T.|.V=C~..
+    '\x6d','\x5c','\xb0','\x82','\xff','\xa3','\xde','\x1f',};
 
 #endif //_H_KEY_DATA_H_
