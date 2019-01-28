d3a169a2d201353aa25979b21080cf1d55cf47e9
Peter <rankstar59@gmail.com>
2019-01-28 16:21:12 +1100

    v1.1.0
     - battery calibration
     - dial codes

--- firmware/bsides2019badge/bsides2019/key.h
+++ firmware/bsides2019badge/bsides2019/key.h
@@ -7,7 +7,7 @@
 
 const uint32_t master_key_size = 24;  // 0x00000018
 const uint8_t master_key_data[24] PROGMEM  = {
-    '\xb6','\x17','\x9b','\x36','\x6c','\x1c','\x54','\x8f','\x7c','\xf7','\x56','\x3d','\x43','\x7e','\x85','\xda', // ...6l.T.|.V=C~..
-    '\x6d','\x5c','\xb0','\x82','\xff','\x53','\x87','\xac',};
+    '\x71','\xef','\x6a','\x5c','\x6c','\x1c','\x54','\x8f','\x7c','\xf7','\x56','\x3d','\x43','\x7e','\x85','\xda', // q.j.l.T.|.V=C~..
+    '\x6d','\x5c','\xb0','\x82','\xff','\x0d','\xf0','\x74',};
 
 #endif //_H_KEY_DATA_H_
